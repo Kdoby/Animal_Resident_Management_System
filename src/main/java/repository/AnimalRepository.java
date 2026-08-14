05456cdcc8a7f1212359b9df27763c97643d211d
doyeon <doyun20445@gmail.com>
2026-08-14 18:01:12 +0900

    feat: add animal repository

---
+++ src/main/java/repository/AnimalRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import model.Animal;
+
+import java.util.List;
+
+public interface AnimalRepository {
+
+    List<Animal> findAll();
+    Animal findById(int id);
+    void save(Animal animal);
+    void update(int id, Animal animal);
+    void delete(int id);
+}

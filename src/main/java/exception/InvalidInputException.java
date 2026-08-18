88510856ffe2396b232a601e462cda6db7e5243b
doyeon <doyun20445@gmail.com>
2026-08-19 00:47:55 +0900

    del: 안쓰는 예외 클래스 삭제

--- src/main/java/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String input) {
-        super(input + "값이 올바르지 않습니다.");
-    }
-}
